973ae8975f8a45ea7e06f168a52c483130939f64
eshwarmolugu <eshwar.1116@gmail.com>
2021-03-22 22:16:04 +0530

    Update Module1Test.java

--- sonarqube-scanner-maven/maven-multimodule/module1/src/test/java/com/acme/module1/Module1Test.java
+++ sonarqube-scanner-maven/maven-multimodule/module1/src/test/java/com/acme/module1/Module1Test.java
@@ -6,7 +6,10 @@ public class Module1Test {
 
   @Test
   public void coveredByUnitTest() {
-    Module1 module1 = new Module1();
-    module1.coveredByUnitTest();
+     assertTrue(false);
+  }
+   @Test
+  public void coveredByUnitTest() {
+     assertTrue(true);
   }
 }
