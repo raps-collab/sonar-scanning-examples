5f005be7dc140cc974b53eeba5a07969ef30cf92
eshwarmolugu <eshwar.1116@gmail.com>
2021-05-10 14:34:03 +0530

    Update One.java

--- sonarqube-scanner-gradle-multimodule/module-one/src/main/java/One.java
+++ sonarqube-scanner-gradle-multimodule/module-one/src/main/java/One.java
@@ -6,6 +6,10 @@ public class One {
 	 public String message4 = "";
 	 public String message5 = "";
 	 public String message6 = "";
+	 public String message7 = "";
+	 public String message8 = ""; 
+	 public String message9 = "";
+	
 	
   public String foo() {
     return "foo";
